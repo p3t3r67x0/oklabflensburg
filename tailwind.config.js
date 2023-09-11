2d7fb49872139f72c59e322c3b6ca859d02f2242
monomorph <github@monomorph.de>
2023-09-11 16:25:56 +0200

    add footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,7 @@ module.exports = {
       colors: {
         black: "#212b36",
         gray: "#3d3d3d",
+        "gray-light": "#dedede",
         dark: "#090E34",
         "dark-700": "#090e34b3",
         primary: "#00367d",
