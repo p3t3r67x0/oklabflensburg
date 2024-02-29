aa20caa31036d8b3d80d8b57f46840626d724351
Aurelius Wendelken <webtobesocial@gmail.com>
2024-02-29 21:47:09 +0100

    moved custom colors to tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./src/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-200': '#13aaa7',
+        'primary-300': '#faa1a3',
+        'primary-400': '#05b657',
+        'primary-500': '#070dfa',
+        'primary-600': '#faa1a3',
+        'primary-700': '#82ded7',
+        'secondary-200': '#388b86'
+      }   
+    }, 
   },
   plugins: [],
 }
